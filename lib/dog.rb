ea639fda150abfbb5c749b4f3e0ede19f852f4b4
Jack Madden <jck13mad@gmail.com>
2020-05-23 17:50:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,8 @@ class Dog
     
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    
+    self
   end
   
   def self.create(hash_of_attributes)
