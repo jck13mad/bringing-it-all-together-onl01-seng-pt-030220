feb1bed43c302e1dc4113353480adf7c45edcab7
Jack Madden <jck13mad@gmail.com>
2020-05-23 17:31:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,7 +10,8 @@ class DOG
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
-        id INTEGER KEY
+        id INTEGER PRIMARY KEY,
+        
       )
     
     
