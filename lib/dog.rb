b46f50697d7c215fe0be4565ccb4c9f57f8a4f10
Jack Madden <jck13mad@gmail.com>
2020-05-23 17:30:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class DOG
   attr_accessor :id, :name, :breed 
   
   def initialize(attributes)
-    attributes.each { |key, value| self.send(("#{key}")) 
+    attributes.each { |key, value| self.send(("#{key}=")) 
     
   
   
