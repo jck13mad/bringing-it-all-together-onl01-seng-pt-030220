f412bcea12d6efc36948b3d7ba4e007c6b9c00e2
Jack Madden <jck13mad@gmail.com>
2020-05-23 17:49:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -90,7 +90,7 @@ class DOG
       UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
     
-    DB[:conn].execute(sql, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
     
     
   
