946a3a6823345f41eb5b1e8f3c89fa31c8a8dd5c
Jack Madden <jck13mad@gmail.com>
2020-05-23 17:36:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,10 @@ class DOG
     SQL
     
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+  
+  
     
     
   
